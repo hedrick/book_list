a4a32779acf2839e291d55d1b7442d439938e4e7
Scott Hedrick <me@scottshedrick.com>
2018-02-16 16:50:35 -0600

    conditional rendering for initial state on book-detail and added new properties to reducer_books

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,10 +3,22 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return (
+        <div>
+          <h3>Select a book to get started.</h3>
+        </div>
+      ) 
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     )
-  }
+  };
 }
 
 function mapStateToProps(state) {
